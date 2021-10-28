fabd87f4eb171caaad6873c6a90bc5b45406ecbf
kulloveth <kulloveth@gmail.com>
2021-10-28 19:04:09 +0100

    add the button functionality

--- app/src/main/java/eu/tutorials/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/eu/tutorials/quizapp/QuizQuestionsActivity.kt
@@ -1,13 +1,17 @@
 package eu.tutorials.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
 
     //Create global variables for the views in the layout
@@ -22,24 +26,26 @@ class QuizQuestionsActivity : AppCompatActivity() {
     /**
      * This function is auto created by Android when the Activity Class is created.
      */
+
+    // TODO 2 A global variables for current position and questions list.
+    // START
+    private var mCurrentPosition: Int = 1 // Default and the first question position
+    private var mQuestionsList: ArrayList<Question>? = null
+    // END
+
+
+    // TODO (STEP 5: A global variables for selected option.)
+    // START
+    private var mSelectedOptionPosition: Int = 0
+    // END
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //This call the parent constructor
         super.onCreate(savedInstanceState)
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.e("Questions Size", "${questionsList.size}")
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
-        // END
-
-        // TODO (Step 3: Setting the question in the UI from the list.)
-        // START
-        val currentPosition = 1 // Default and the first question position
-        val question: Question? =
-            questionsList[currentPosition - 1] // Getting the question from the list with the help of current position.
 
         progressBar=findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progress)
@@ -49,18 +55,132 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+
+        // TODO (STEP 3: Make the questions list and the current position variable global and remove the logs here.)
+        // START
+        mQuestionsList = Constants.getQuestions()
+        // END
+
+        setQuestion()
+
+        // TODO (STEP 4: Set all the click events for Options using the interface onClick listener)
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+    }
+
+
+
+    // TODO (STEP 1 : Lets create a function to set the question in the UI components which we have done earlier the onCreate method. And make some of the variables global which we will be using later.)
+    // START
+    /**
+     * A function for setting the question to UI components.
+     */
+    private fun setQuestion() {
+
+        val question: Question =
+            mQuestionsList!![mCurrentPosition - 1] // Getting the question from the list with the help of current position.
+
         progressBar?.progress =
-            currentPosition // Setting the current progress in the progressbar using the position of question
+            mCurrentPosition // Setting the current progress in the progressbar using the position of question
         tvProgress?.text =
-            "$currentPosition" + "/" + progressBar?.getMax() // Setting up the progress text
+            "$mCurrentPosition" + "/" + progressBar?.max // Setting up the progress text
 
         // Now set the current question and the options in the UI
-        tvQuestion?.text = question!!.question
+        tvQuestion?.text = question.question
         ivImage?.setImageResource(question.image)
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+
+            }
+
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+
+            }
+
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+
+            }
+
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+
+            }
+        }
+    }
+
+    // TODO (STEP 6: Create a function for view for highlighting the selected option.)
+    // START
+
+    /**
+     * A function to set the view of selected option view.
+     */
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+
+        defaultOptionsView()
+
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(
+            Color.parseColor("#363A43")
+        )
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this@QuizQuestionsActivity,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+
+    // TODO (STEP 8: Create a function to set default options view.)
+    // START
+    /**
+    * A function to set default options view when the new question is loaded or when the answer is reselected.
+    */
+    private fun defaultOptionsView() {
+
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3,it)
+        }
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this@QuizQuestionsActivity,
+                R.drawable.default_option_border_bg
+            )
+        }
     }
 }
 // END
