406ed2801d2bc3a8fbe9d0fe35b8604f0a0393cf
kulloveth <kulloveth@gmail.com>
2021-10-28 12:51:24 +0100

    prepare the project style and manifest

--- app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
@@ -1,11 +1,7 @@
 package eu.tutorials.quizapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatEditText
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonStart:Button = findViewById(R.id.btn_start)
-        val etName:AppCompatEditText = findViewById(R.id.et_name)
-        buttonStart.setOnClickListener {
-          if (etName.text.toString().isEmpty()){
-              Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
-          }else{
-              val intent = Intent(this,QuizQuestionsActivity::class.java)
-              startActivity(intent)
-          }
-        }
+
     }
 }
